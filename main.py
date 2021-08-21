754da42ae21735ec098cc1343cfd92ba179c7dc6
Aviv <67284525+avivk9@users.noreply.github.com>
2021-08-21 18:28:02 +0300

    path function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #proves Collatz Conjecture for given range of numbers
 
 #enter amount of numbers
-RANGE = 99999
+RANGE = 2
 
 def Collatz_Conjecture(n):
     while n != 1:
@@ -11,6 +11,18 @@ def Collatz_Conjecture(n):
             n = (n*3) + 1
     if n == 1:
         return True
+
+def Collatz_Conjecture_Path(n):
+    path = str(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n/2
+            path += ("->" + str(n))
+        else:
+            n = (n*3) + 1
+            path += ("->" + str(n))
+    if n == 1:
+        return path
 
 if __name__ == "__main__":
     for n in range(1, RANGE):
